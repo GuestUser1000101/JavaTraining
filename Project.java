59d2c930238be118d9aea7935c3bd4c06d651bb2
GuestUser1000101 <122332978+GuestUser1000101@users.noreply.github.com>
2024-11-14 18:10:14 -0800

    Add project file

---
+++ Project.java
@@ -0,0 +1,37 @@
+package Classes;
+
+/** 
+ * https://bit.ly/nrg-java-training
+ * 
+ * Create a Student class with fields:
+ * int age
+ * int class (9 - 12)
+ * String favoriteSubject
+ * double gpa (0 - 4)
+ * boolean canGraduate
+ * double happiness
+ * 
+ * And also keep track of the total number of students
+ * 
+ * Create a constructor that takes favoriteSubject, and gpa
+ * Create setters and getters for each field
+ * CanGraduate is true if and only if gpa is greater than or equal to 2 and class is 12
+ * class defaults to 9
+ * age defaults to 13
+ * happiness defaults to 5
+ * 
+ * Create a method failClass(String subject) that will reduce gpa by 0.5 and if subject == favoriteSubject, reduce happiness by 1
+ * Create a method playGamesDuringClass() that will reduce gpa by 0.2 and increase happiness by 0.1
+ * Create a method doHomework() that will increase gpa by 0.1 and reduce happiness by 0.2
+ * Create a method age() that will increase age and class by 1, and reduce happiness by 0.1
+ * 
+ * Create any other methods you feel appropriate to this class.
+ */
+class Student {
+}
+
+public class Project {
+    public static void main(String[] args) {
+        // Test your code here!
+    }
+}
